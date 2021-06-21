8ee92d31193ff0671160e2536c0009c9d6354ee8
Netanel <ne.zaffran@gmail.com>
2021-06-21 22:40:00 +0300

    add apiDuckduckgoSlice

---
+++ client/src/reducers/apiDuckduckgoSlice.ts
@@ -0,0 +1,54 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios, { AxiosRequestConfig } from 'axios';
+import { RelatedTopic, SearchModel } from '../models/searchModels';
+
+interface ApiDuckduckgoState {
+  searchResult: RelatedTopic[];
+  isPending: boolean;
+  isRejected: boolean;
+}
+
+const initialState: ApiDuckduckgoState = {
+  searchResult: [],
+  isPending: false,
+  isRejected: false,
+};
+
+interface SearchApiDuckduckgoPayload {
+  response: RelatedTopic[];
+  error: any;
+}
+
+export const searchApiDuckduckgo = createAsyncThunk<
+  SearchApiDuckduckgoPayload,
+  { text: string }
+>('apiDuckduckgo/search', async (text) => {
+  try {
+    const config: AxiosRequestConfig = { params: { q: text, format: 'json' } };
+    const response = await axios.get<SearchModel>(`http://api.duckduckgo.com/`, config);
+    return { response: response.data.RelatedTopics as RelatedTopic[], error: false };
+  } catch (e) {
+    return { error: true } as any;
+  }
+});
+
+const apiDuckduckgoSlice = createSlice({
+  name: 'apiDuckduckgo',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(searchApiDuckduckgo.pending, (state, action) => {
+      return { ...state, isPending: true };
+    });
+    builder.addCase(searchApiDuckduckgo.rejected, (state, action) => {
+      return { ...state, isPending: false, isRejected: true };
+    });
+    builder.addCase(searchApiDuckduckgo.fulfilled, (state, action) => {
+      if (!action.payload.error)
+        return { ...state, isPending: false, searchResult: action.payload.response };
+      else return { ...state };
+    });
+  },
+});
+
+export default apiDuckduckgoSlice.reducer;
--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
+import apiDuckduckgoReducer from './apiDuckduckgoSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    apiDuckduckgo: apiDuckduckgoReducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
